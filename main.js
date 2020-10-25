b8e815f2e4f71077e23f663d4a9c88239ed9d0de
ravlick <svtrojan16@gmail.com>
2020-10-25 22:44:20 +0200

    end plugin

---
+++ main.js
@@ -0,0 +1,12 @@
+// 'use strict';
+// function draw(){
+//     let canvas = document.getElementById('blue');
+//     if (canvas.getContext){
+//         let ctx = canvas.getContext('2d');
+//         ctx.beginPath();
+//         ctx.moveTo(75,50);
+//         ctx.lineTo(100,75);
+//         ctx.lineTo(100,25);
+//         ctx.fill();
+//     }
+// }
